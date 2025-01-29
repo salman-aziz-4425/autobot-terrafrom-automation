1746580f95a3069f5e62cb60a5920743a665e785
SalmanAziz <salmanaziz@Salman-Aziz.local>
2025-01-29 14:05:18 +0500

    Exclude .terraform directory from Git tracking

---
+++ backend.tf
@@ -0,0 +1,8 @@
+# This file is used for backend configuration
+terraform {
+  backend "gcs" {
+    bucket = "autobot_scheduler_cloud_function"
+    prefix = "terraform/state"
+    credentials = "/Users/salmanaziz/.config/gcloud/application_default_credentials.json"
+  }
+} 
